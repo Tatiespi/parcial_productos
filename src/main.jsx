a8c89e70881c4f383696df046521d8ac3d7f8ddd
tatiespi <tatiana.espinosa@uao.edu.co>
2023-04-18 19:52:28 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Post from '../Parcial2/Post'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Post />
   </React.StrictMode>,
 )
